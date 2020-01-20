0881cfc3cb9e92df6b0b3250b2c5485bc27ec7e7
PrestonCazier <prestoncazier@gmail.com>
2020-01-20 15:39:33 -0800

    implement group object

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ import sys
 
 numagents = [None,4,3,2,2]
 names = {'yellow':'Knights of the Shield', 'grey':'City Guard', 'blue':'Silverstars', 'green':'Harpers', 'red':'Red Sashes'}
+
 class Player:
     lord = None
     quests = []
@@ -61,9 +62,42 @@ class Player:
         return
 
 class Group():
-    def __init__(self, numplayers, numai, colors = ['yellow','grey','blue','green','red']):
-        self.names = colors
-        self.players = {}
-        for i in numplayers:
-            self.players[colors[i]] = Player(None, numagents[numplayers], None, None)
-        self.first = colors[0]
+    #AI models will be tied to a player color,
+    # order will be randomized?
+    #first is a color
+    def __init__(self, numplayers, numai, colors = ['yellow','grey','blue','green','red'], pcs = []):
+        if len(colors) != numplayers:
+            print('ERROR player.py line 70 Group __init__ list length mismatch')
+            return
+        if numplayers != numai + len(pcs):
+            print('ERROR player.py line 73 Group __init__ group size mismatch')
+        
+        self.numplayers = numplayers
+        for pc in pcs:
+            if pc.color not in colors:
+                print('ERROR')
+                return
+            colors.remove(pc.color)
+        self.colors = colors
+        self.players = [Player(color, numagents[numplayers]) for color in self.colors]
+        self.players.extend(pcs)
+        self.first = 0
+        self.current = self.first
+    
+    def getSpecific(self, color):
+        return self.players[self.colors.index(color)]
+
+    def getCurrent(self):
+        return self.players[self.current]
+
+    def goTo(self, color):
+        self.current = self.colors.index(color)
+
+    def goToFirst(self):
+        self.current = self.first
+
+    def goToLast(self):
+        self.current = self.numplayers - 1
+    
+    def nextPlayer(self):
+        current = self.current + 1 if self.current < self.numplayers else 0
