1a1969d14d3a0c035a0eca77194f22555268eb20
PrestonCazier <prestoncazier@gmail.com>
2020-01-21 14:34:18 -0800

    convert to use resource vector class

--- quest.py
+++ quest.py
@@ -30,45 +30,24 @@
 # type
 
 import pandas as pd
+from resourcevector import RVector
 
-typeslist = ['commerce', 'skullduggery', 'warfare', 'piety', 'arcana']
+typeslist = ['Commerce', 'Skullduggery', 'Warfare', 'Piety', 'Arcana', 'Mandatory']
 
 class Quest:
     name = "unnamed quest"
-    plotquest = False
-    mandatory = False
-    coincost = 0
-    coinreward = 0
-    whitecost = 0
-    whitereward = 0
-    blackcost = 0
-    blackreward = 0
-    orangecost = 0
-    orangereward = 0
-    purplecost = 0
-    purplereward = 0
-    vpreward = 0
-    intrigue = 0
 
     #def __init__(self, *args, **kwargs):
         #super().__init__(*args, **kwargs)
-    def __init__(self, questname, questtype, cc, cr, wc, wr, bc, br, onc, onr, pc, pr, vp, plot, intrigue, verbose=False):
+    def __init__(self, questname, questtype, plot, cost, reward, verbose=False):
         self.name = questname
         self.questtype = questtype
-        self.coincost = int(cc)
-        self.coinreward = int(cr)
-        self.whitecost = int(wc)
-        self.whitereward = int(wr)
-        self.blackcost = int(bc)
-        self.blackreward = int(br)
-        self.orangecost = int(onc)
-        self.orangereward = int(onr)
-        self.purplecost = int(pc)
-        self.purplereward = int(pr)
-        self.vpreward = int(vp)
+        self.mandatory = questtype == 'Mandatory'
         self.plotquest = plot
-        self.intrigue = int(intrigue)
-        if verbose:
+        self.cost = RVector()
+        self.reward = RVector()
+
+        """ if verbose:
             print('Name: '+ str(self.name) + ' is a ' + str(type(self.name)))
             print('QuestType: '+ str(self.questtype) + ' is a ' + str(type(self.questtype)))
             print('CoinCost: '+ str(self.coincost) + ' is a ' + str(type(self.coincost)))
@@ -83,12 +62,12 @@ class Quest:
             print('PurpleReward: '+ str(self.purplereward) + ' is a ' + str(type(self.purplereward)))
             print('VPReward: '+ str(self.vpreward) + ' is a ' + str(type(self.vpreward)))
             print('PlotQuest: '+ str(self.plotquest) + ' is a ' + str(type(self.plotquest)))
-            print('Intrigue: '+ str(self.intrigue) + ' is a ' + str(type(self.intrigue)))
+            print('Intrigue: '+ str(self.intrigue) + ' is a ' + str(type(self.intrigue))) """
         return
 
     def __repr__(self):
         cost = []
-        if self.coincost > 0:
+        """ if self.coincost > 0:
             cost.append(str(self.coincost) + ' coin' + 's' if self.coincost > 1 else '')
         if self.whitecost > 0:
             cost.append(str(self.whitecost) + ' cleric' + ('s' if self.whitecost > 1 else ''))
@@ -116,7 +95,8 @@ class Quest:
         print(cost)
         print(reward)
         
-        return "this is a " + self.questtype +  ' quest that costs ' + ', '.join(cost) + ' and rewards ' + ', '.join(reward) +'\n'
+        return "this is a " + self.questtype +  ' quest that costs ' + ', '.join(cost) + ' and rewards ' + ', '.join(reward) +'\n' """
+        return
 
 class Deck(list):
 
@@ -124,11 +104,29 @@ class Deck(list):
     shufflecallback = []
 
     def __init__(self, iterable):
-        questdf = pd.read_csv('quest_cards.csv')
-        quests = [Quest(questdf.iloc[i, 0], questdf.iloc[i, 1], questdf.iloc[i, 2], questdf.iloc[i, 3], 
-                        questdf.iloc[i, 4], questdf.iloc[i, 5], questdf.iloc[i, 6], questdf.iloc[i, 7], 
-                        questdf.iloc[i, 8], questdf.iloc[i, 9], questdf.iloc[i, 10], questdf.iloc[i, 11], 
-                        questdf.iloc[i, 12], questdf.iloc[i, 13], questdf.iloc[i, 14], True) for i in range(35)]
+        qdf = pd.read_csv('quest_cards.csv')
+        names = qdf['name'].tolist()
+        questtypes = qdf['questtype'].tolist()
+        plots = qdf['plot'].tolist()
+        ccs = qdf['cc'].tolist()
+        wcs = qdf['wc'].tolist()
+        bcs = qdf['bc'].tolist()
+        oncs = qdf['onc'].tolist()
+        pcs = qdf['pc'].tolist()
+        crs = qdf['cr'].tolist()
+        wrs = qdf['wr'].tolist()
+        brs = qdf['br'].tolist()
+        onrs = qdf['onr'].tolist()
+        prs = qdf['pr'].tolist()
+        vps = qdf['vp'].tolist()
+        intrigues = qdf['intrigue'].tolist()
+        qs = qdf['quest'].tolist()
+        choices = qdf['choice'].tolist()
+        costs = [RVector(ccs[i], wcs[i], bcs[i], oncs[i], pcs[i], 0, 0, 0, 0) for i in range(len(names))]
+        rewards = [RVector(crs[i], wrs[i], brs[i], onrs[i], prs[i], vps[i], intrigues[i], qs[i], choices[i]) for i in range(len(names))]
+        quests = [Quest(names[i], questtypes[i], plots[i], costs[i], rewards[i], True) for i in range(len(names))]
+        mandatory = [q for q in quests if q.questtype == 'Mandatory']
+        quests.remove(mandatory)
 
     def __repr__(self):
         return super().__repr__()
